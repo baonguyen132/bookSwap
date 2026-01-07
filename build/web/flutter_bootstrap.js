a3d2b123cae51c71e5f0bcddef96944aecaaf820
Bảo Nguyên <118587919+baonguyen132@users.noreply.github.com>
2026-01-07 19:08:46 +0700

    upload

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1515284587"
+    serviceWorkerVersion: "4031596070"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -5,7 +5,6 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"assets/AssetManifest.bin": "bc7a73e931b15f4bfb0e5d99e2955f4f",
 "assets/AssetManifest.bin.json": "e8d1631709ab78c6891aa40c07dfadb0",
-"assets/AssetManifest.json": "f188830e9406c75c67b5b13e09e41e64",
 "assets/assets/images/anhsach1.jpeg": "71973b9c4682ddfa7988686c251c7ad6",
 "assets/assets/images/anhsach2.jpeg": "379de220bd3343ebd375aacfd537d0a0",
 "assets/assets/images/anhsach3.jpeg": "8182613ad17a45ee5f14f8413b4bef80",
@@ -34,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "bc7a73e931b15f4bfb0e5d99e2955f4f
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "cdcb18c25eee9f69cf6b2d91185f53f1",
+"flutter_bootstrap.js": "f64e62cb5bc8ddd509ec78191c021e7d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
